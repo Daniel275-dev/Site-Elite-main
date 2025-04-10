9c29dd8c3c122302b58ec4d6bc36d9804d5c005b
Daniel275-dev <daniellopesvieira275@gmail.com>
2025-04-10 19:53:58 -0300

    mudança layout

---
+++ script.js
@@ -0,0 +1,60 @@
+// função limpar cadastro
+function limparFormulario(){
+    let elementos = document.getElementsByTagName("input")
+    let el = [...elementos]
+    el.forEach(element => {
+         element.checked = '' 
+     });
+     let valores = document.getElementsByTagName("input");
+     let limpar = [...valores]
+     limpar.forEach(valor => {
+         valor.value = ''
+     });
+    }
+// correções de caracteres //
+
+    // Telefone tela de cadastro//
+  const telefoneInput = document.getElementById('telefone');
+
+  telefoneInput.addEventListener('input', function (e) {
+    let valor = e.target.value;
+
+    valor = valor.replace(/\D/g, '');
+
+    if (valor.length > 0) {
+      valor = '(' + valor;
+    }
+    if (valor.length > 3) {
+      valor = valor.slice(0, 3) + ') ' + valor.slice(3);
+    }
+    if (valor.length > 10) {
+      valor = valor.slice(0, 10) + '-' + valor.slice(10);
+    }
+
+    valor = valor.slice(0, 15);
+
+    e.target.value = valor;
+  });
+// casdastro de CPF //
+const cpfInput = document.getElementById('cpf');
+
+cpfInput.addEventListener('input', function (e) {
+  let valor = e.target.value;
+
+  valor = valor.replace(/\D/g, '');
+
+  if (valor.length > 3) {
+    valor = valor.slice(0, 3) + '.' + valor.slice(3);
+  }
+  if (valor.length > 7) {
+    valor = valor.slice(0, 7) + '.' + valor.slice(7);
+  }
+  if (valor.length > 11) {
+    valor = valor.slice(0, 11) + '-' + valor.slice(11);
+  }
+
+  valor = valor.slice(0, 14);
+
+  e.target.value = valor;
+});
+// code carrosel
